cfe02c92186f56cad27e10dc7c914db1c15a05cd
pckrishnadas88 <pckrishnadas88@gmail.com>
2022-12-14 16:10:27 +0530

    migrated all js methods to ts file.

--- JSONQuery.ts
+++ JSONQuery.ts
@@ -1,28 +1,22 @@
-enum ConditionsEnum {
-    EqualTo = "==",
-    LessThan = "<",
-    GreaterThan = ">",
-    lessThanOrEqual = "<=",
-    GreaterThanOrEqual = ">=",
-    NotEqual = "!="
-}
+type SortOrder = "asc" | "desc"
+type ComparisonOperator = "==" | "<" | ">" | "<=" | ">=" | "!="
 export class JSONQuery<DataType> {
     data: DataType[]
     result: DataType[] = []
     constructor(data: DataType[]) {
-      this.data = data 
+        this.data = data
     }
-   
+
     get(): DataType[] {
         return this.result
     }
     select(columns: Array<keyof DataType>) {
-        if(columns.length == 1 && columns[0] == "*") {
+        if (columns.length == 1 && columns[0] == "*") {
             this.result = this.data
             return this
         }
-        this.data.map( e => {
-            const singleRow:DataType = <DataType>{}
+        this.data.map(e => {
+            const singleRow: DataType = <DataType>{}
             for (const column of columns) {
                 singleRow[column as keyof DataType] = e[column as keyof DataType]
             }
@@ -30,40 +24,74 @@ export class JSONQuery<DataType> {
         })
         return this
     }
-    limit(limit=10) {
-        if(this.result.length >= limit) {
+    limit(limit = 10) {
+        if (this.result.length >= limit) {
             this.result = this.result.slice(0, limit)
         }
         return this
     }
-    where(column:  keyof DataType, condition: ConditionsEnum, value: any) {
-        this.result = this.result.filter(e=> {
-            if(condition == ConditionsEnum.EqualTo) {
+    where(column: keyof DataType, condition: ComparisonOperator, value: any) {
+        this.result = this.result.filter(e => {
+            if (condition == "==") {
                 return e[column as keyof DataType] == value
-            } else if(condition == ConditionsEnum.GreaterThan) {
+            } else if (condition == ">") {
                 return e[column as keyof DataType] > value
-            } else if(condition == ConditionsEnum.LessThan) {
+            } else if (condition == "<") {
                 return e[column as keyof DataType] < value
-            } else if(condition == ConditionsEnum.NotEqual) {
+            } else if (condition == "!=") {
                 return e[column as keyof DataType] != value
-            } else if(condition == ConditionsEnum.GreaterThanOrEqual) {
+            } else if (condition == ">=") {
                 return e[column as keyof DataType] >= value
-            } else if(condition == ConditionsEnum.lessThanOrEqual) {
+            } else if (condition == "<=") {
                 return e[column as keyof DataType] <= value
             }
 
-         } )
+        })
         return this
+    }
+    orderBy(column: keyof DataType, sort_order:SortOrder) {
+        if (sort_order.toLowerCase() == 'asc') {
+            this.result = this.result.sort((a, b) => {
+                const nameA = String(a[column as keyof DataType]).toLowerCase() // ignore upper and lowercase
+                const nameB = String(b[column as keyof DataType]).toLowerCase() // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return -1
+                }
+                if (nameA > nameB) {
+                    return 1
+                }
+
+                // names must be equal
+                return 0;
+            })
+        } else if (sort_order.toLowerCase() == 'desc') {
+            this.result = this.result.sort((a, b) => {
+                const nameA = String(a[column as keyof DataType]).toLowerCase() // ignore upper and lowercase
+                const nameB = String(b[column as keyof DataType]).toLowerCase() // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return 1;
+                }
+                if (nameA > nameB) {
+                    return -1;
+                }
+
+                // names must be equal
+                return 0;
+            })
+        }
+        return this
+
+
     }
 }
 
 var data = {
     people: [
-      {name: 'Matt', country: 'NZ', age:34},
-      {name: 'Pete', country: 'AU', age:20},
-      {name: 'Mikey', country: 'NZ', age: 31},
-      {name: 'Kevin', country: 'AU', age:40},
-      {name: 'Joseph', country: 'AU', age:43},
+        { name: 'Matt', country: 'NZ', age: 34 },
+        { name: 'Pete', country: 'AU', age: 20 },
+        { name: 'Mikey', country: 'NZ', age: 31 },
+        { name: 'Kevin', country: 'AU', age: 40 },
+        { name: 'Joseph', country: 'AU', age: 43 },
 
     ]
 }
@@ -73,13 +101,12 @@ interface People {
     country: string;
     age: number;
 }
-const qObj = new JSONQuery<People>(data.people)
+const qObj = new JSONQuery(data.people)
 console.log(
     qObj
-    .select(['name', 'age'])
-    //.select({name:})
-    .where("age", ConditionsEnum.NotEqual, 40)
-    // //.where("name", "==", "Matt")
-    // .orderBy("age", "desc")
-    .limit(2)
-    .get())
+        .select(['name', 'age'])
+        .where("age", "==", 40)
+        // //.where("name", "==", "Matt")
+        .orderBy("age", "asc")
+        .limit(4)
+        .get())
