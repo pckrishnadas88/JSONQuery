0218e554683c6fc6c5342b969dee97d027abb6d3
pckrishnadas88 <pckrishnadas88@gmail.com>
2022-12-15 16:35:23 +0530

    updated example.

--- example.ts
+++ example.ts
@@ -11,10 +11,22 @@ var data = {
 }
 
 const qObj = new JSONQuery(data.people)
-console.log(
-    qObj
-        .select(['name', 'age'])
-        //.distinct("country")
-        .orderBy("country", "asc")
-        .fetchOnly("name")
-)
+// console.log(
+//     qObj
+//         //.select(['name', 'age'])
+//         .distinct("country")
+//         .fetchOnly("country")
+// )
+
+
+var ageArray = 
+    [
+        {"name":"Joe", "age":17}, 
+        {"name":"Bob", "age":17}, 
+        {"name":"Carl", "age": 35}
+    ]
+
+const ageArrayObj = new JSONQuery(ageArray)
+
+console.log(ageArrayObj.distinct("age").get()) // outputs: [ { name: 'Bob', age: 17 }, { name: 'Carl', age: 35 } ]
+console.log(ageArrayObj.distinct("age").fetchOnly("age")) // outputs: [ 17, 35 ]
